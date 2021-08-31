1d40dce22efb68a8a4b9cd98628066d3ffed2d2d
sdarwin <samuel.d.darwin@gmail.com>
2021-08-31 18:41:05 +0000

    full_sync and remove_objects functions

--- restmote/sync.py
+++ restmote/sync.py
@@ -6,6 +6,7 @@ import re
 
 from django.conf import settings
 
+restmote_remote_ids=[]
 
 root = urljoin(settings.RESTMOTE_HOST + ":" + settings.RESTMOTE_PORT, settings.RESTMOTE_API_ROOT)
 
@@ -27,11 +28,15 @@ def get_data(url):
         return False, []
 
 
-def build_objects(obj_class, obj_string, data, field_bindings, nested=[]):
+def build_objects(obj_class, data, field_bindings, remote_id='id', local_id='id', nested=[]):
+    global restmote_remote_ids
     for e in data:
 
+        if e[remote_id] not in restmote_remote_ids:
+            restmote_remote_ids.append(e[remote_id])
+
         try:
-            o = obj_class.objects.get(**{'id' + obj_string: e["id"]})
+            o = obj_class.objects.get(**{local_id: e[remote_id]})
         except obj_class.DoesNotExist:
             o = obj_class()
 
@@ -42,12 +47,12 @@ def build_objects(obj_class, obj_string, data, field_bindings, nested=[]):
             for f in [x for x in e[n] if x in field_bindings]:
                 setattr(o, field_bindings[f], e[n][f])
 
-        setattr(o, "id" + obj_string, e["id"])
+        setattr(o, local_id, e[remote_id])
         o.save()
-        logging.info("Added %s: %s" % (obj_string, o.pk))
+        logging.info("Added %s: %s" % (local_id, o.pk))
 
 
-def sync_objects(url, qfilter, obj_class, obj_string, field_bindings, nested=[]):
+def sync_objects(url, qfilter, obj_class, field_bindings, remote_id='id', local_id='id', nested=[]):
     if '?' in url:
         querytoken='&';
     else:
@@ -59,16 +64,20 @@ def sync_objects(url, qfilter, obj_class, obj_string, field_bindings, nested=[])
 
     status, data, nexturl = get_data(targeturl + querytoken + qfilter)
     if status:
-        build_objects(obj_class, obj_string, data, field_bindings, nested)
+        build_objects(obj_class, data, field_bindings, remote_id=remote_id, local_id=local_id, nested=nested)
         if nexturl:
-            sync_objects(nexturl, qfilter, obj_class, obj_string, field_bindings, nested=[])
+            sync_objects(nexturl, qfilter, obj_class, field_bindings, remote_id=remote_id, local_id=remote_id , nested=[])
         return 1
     else:
         return 0
 
-
-def remove_objects(url, obj_class, obj_string):
-    status, remote_ids = get_data(root + url)
+def remove_objects_v1(url, obj_class, obj_string):
+    """
+    The 'original' version of remove_objects.
+    Didn't take pagination of results into account. Seemed to have other bugs also.
+    Use full_sync and new remove_objects instead.
+    """
+    status, remote_ids, nexturl = get_data(root + url)
     if status:
         local_ids = obj_class.objects.values_list('id' + obj_string, flat=True)
         must_remove = list(set(local_ids).difference(remote_ids))
@@ -78,3 +87,20 @@ def remove_objects(url, obj_class, obj_string):
         return 1
     else:
         return 0
+
+def remove_objects(obj_class, local_id='id'):
+    """
+    Prune items from database table after a sync.
+    Because this is comparing against discovered remote values, it must be run in conjunction with sync_objects. Thus, calling it from within full_sync.
+    """
+    local_ids = obj_class.objects.values_list(local_id, flat=True)
+    must_remove = list(set(local_ids).difference(restmote_remote_ids))
+    obj_class.objects.filter(**{local_id + '__in': must_remove}).delete()
+    if must_remove:
+        logging.info("Deleted %s: %s" % (local_id, ', '.join(str(x) for x in must_remove)))
+
+def full_sync(url, qfilter, obj_class, field_bindings, remote_id='id', local_id='id', nested=[]):
+    global restmote_remote_ids
+    restmote_remote_ids=[]
+    sync_objects(url, qfilter, obj_class, field_bindings, remote_id=remote_id, local_id=local_id, nested=[])
+    remove_objects(obj_class, local_id=local_id)
